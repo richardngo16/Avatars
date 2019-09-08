756b886fae4ae60a41d253ad0ea53ebbf441f04e
richardngo16 <richardngo16@gmail.com>
2019-09-08 13:12:24 -0700

    Started on hair data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
 import React, { useState } from "react";
 import Webcam from "react-webcam";
-import { getFaceData } from "./utils/util";
+import { getFaceData, mapHairData } from "./utils/util";
 import Button from "@material-ui/core/Button";
+import Avatar from "avataaars";
 
 // TODO Split this file into different components
 // Use contextapi if needed
+
 const videoConstraints = {
-  width: 300,
-  height: 300,
   facingMode: "user"
 };
 
 const App = () => {
+  //states
+  const [uiState, setUiState] = useState("camera");
+  const [apiResponseData, setApiResponseData] = useState([]);
   const webcamRef = React.useRef(null);
-  const capture = React.useCallback(() => {
+  const capture = React.useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
-    getFaceData(imageSrc);
+    setUiState("loading");
+    const data = await getFaceData(imageSrc);
+    console.log(data);
+    setApiResponseData(data);
+    setUiState("avatar");
   }, [webcamRef]);
 
-  return (
-    <div
-      className="App"
-      style={{ width: "100%", height: "100%", textAlign: "center" }}
-    >
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-      />
+  const CameraMode = () => {
+    return (
       <div>
-        <Button variant="contained" color="primary" onClick={capture}>
-          Capture Photo
-        </Button>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          height={300}
+          width={"100%"}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
+        <div>
+          <Button variant="contained" color="primary" onClick={capture}>
+            Capture Photo
+          </Button>
+        </div>
       </div>
+    );
+  };
+
+  const LoadingScreen = () => {
+    return (
+      <div>
+        <h1>Please Wait ...</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App" style={{ textAlign: "center" }}>
+      {uiState === "camera" && <CameraMode />}
+      {uiState === "loading" && <LoadingScreen />}
+      {uiState === "avatar" && (
+        <Avatar
+          topType={mapHairData(apiResponseData)}
+          clotheType="Hoodie"
+          clotheColor="Black"
+          eyeType="Default"
+          eyebrowType="Default"
+        />
+      )}
     </div>
   );
 };
--- src/utils/util.js
+++ src/utils/util.js
@@ -17,10 +17,50 @@ const betaFaceRequest = imageSrc => {
 const getFaceData = async imageSrc => {
   try {
     const data = await (await betaFaceRequest(imageSrc)).json();
-    return data;
+    return data.media.faces[0].tags;
   } catch (e) {
     console.error(e);
   }
 };
 
+// Takes in Array of Data
+// {
+//   "name": "5oclock shadow",
+//   "value": "no",
+//   "confidence": 0.43
+// }
+export const mapHairData = apiDataResponse => {
+  const wearingHat = apiDataResponse.filter(field => {
+    return field.name === "wearing hat";
+  })[0].value;
+
+  console.log("Wearing Hat?", wearingHat);
+  if (wearingHat === "yes") {
+    console.log("yes hat");
+    return "Hat";
+  }
+
+  const noHairCheck = apiDataResponse.filter(field => {
+    return field.name === "bald";
+  })[0];
+
+  console.log(
+    "NoHair?",
+    noHairCheck.value,
+    "Confidence?",
+    noHairCheck.confidence
+  );
+
+  if (noHairCheck.value === "yes" && noHairCheck.confidence >= 0.9) {
+    return "NoHair";
+  }
+
+  //none, very short, short, average, long, very long
+  const hairLength = apiDataResponse.filter(field => {
+    return field.name === "hair length";
+  })[0].value;
+  console.log("hair length", hairLength);
+
+  return "ShortHairFrizzle";
+};
 export { getFaceData };
