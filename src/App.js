def44343b4149551bb320c4ca328122e519483fe
richardngo16 <richardngo16@gmail.com>
2019-09-14 14:34:51 -0700

    Separated Camera Mode

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React, { useState } from "react";
-import Webcam from "react-webcam";
-import { getFaceData } from "./utils/util";
-import Button from "@material-ui/core/Button";
 import UserAvatar from "./components/UserAvatar";
+import CameraMode from "./components/CameraMode";
 
 // TODO Split this file into different components
 // Use contextapi if needed
-
-const videoConstraints = {
-  facingMode: "user"
-};
-
 const App = () => {
   //states
   const [uiState, setUiState] = useState("camera");
   const [apiResponseData, setApiResponseData] = useState([]);
   const [screenshot, setScreenshot] = useState("");
-  const webcamRef = React.useRef(null);
-  const capture = React.useCallback(async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setScreenshot(imageSrc);
-    setUiState("loading");
-    const data = await getFaceData(imageSrc);
-    setApiResponseData(data);
-    setUiState("avatar");
-  }, [webcamRef]);
-
-  const CameraMode = () => {
-    return (
-      <div>
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          height={300}
-          width={"100%"}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-        />
-        <div>
-          <Button variant="contained" color="primary" onClick={capture}>
-            Capture Photo
-          </Button>
-        </div>
-      </div>
-    );
-  };
 
   const LoadingScreen = () => {
     return (
@@ -56,7 +20,13 @@ const App = () => {
 
   return (
     <div className="App" style={{ textAlign: "center" }}>
-      {uiState === "camera" && <CameraMode />}
+      {uiState === "camera" && (
+        <CameraMode
+          setUiState={setUiState}
+          setScreenshot={setScreenshot}
+          setApiResponseData={setApiResponseData}
+        />
+      )}
       {uiState === "loading" && <LoadingScreen />}
       {uiState === "avatar" && (
         <UserAvatar apiResponseData={apiResponseData} screenshot={screenshot} />
